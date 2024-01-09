75cdc2da550affdcc26a6c583fb0c383a1a91c3c
Ruben Meza <rmezar@gmail.com>
2024-01-09 12:57:08 -0600

    new plugins nvim

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -1,10 +1,13 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-g>", ui.toggle_quick_menu)
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
 
-vim.keymap.set("n", "<C-z>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-x>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-c>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-v>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-g>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-z>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-x>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-c>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-v>", function() harpoon:list():select(4) end)
--- nvim/.config/nvim/lua/polluelo/packer.lua
+++ nvim/.config/nvim/lua/polluelo/packer.lua
@@ -20,26 +20,53 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
+    use "nvim-lua/plenary.nvim"
+
     use {
-	    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+      'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	    -- or                            , branch = '0.1.x',
 	    requires = { {'nvim-lua/plenary.nvim'} }
     }
 
     use('EdenEast/nightfox.nvim')
 
+    -- use({
+    --   'rose-pine/neovim',
+    --   as = 'rose-pine',
+    --   config = function()
+    --     vim.cmd('colorscheme rose-pine')
+    --   end
+    -- })
+
+    use({
+      "folke/trouble.nvim",
+      config = function()
+        require("trouble").setup {
+          icons = false,
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end
+    })
+
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
     use("nvim-treesitter/nvim-treesitter-context")
 
-    use('ThePrimeagen/harpoon')
+    use {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      requires = { {"nvim-lua/plenary.nvim"} }
+    }
 
     use('mbbill/undotree')
 
     use('tpope/vim-fugitive')
 
     use {
-	    'VonHeikemen/lsp-zero.nvim',
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v1.x',
 	    requires = {
 		    -- LSP Support
 		    {'neovim/nvim-lspconfig'},
@@ -75,7 +102,7 @@ return require('packer').startup(function(use)
       'lewis6991/gitsigns.nvim',
     }
 
-    -- use('fatih/vim-go')
+    use('fatih/vim-go')
 
     -- use('folke/flash.nvim')
 end)
